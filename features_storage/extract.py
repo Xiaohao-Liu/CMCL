145fda63b0257534e725ee7861a9fa289f3d66ad
Xiaohao Liu <xiaohao.liu@hotmail.com>
2025-12-18 22:46:53 +0800

    add class label extraction

--- features_storage/extract.py
+++ features_storage/extract.py
@@ -3,6 +3,7 @@ import torch
 from torch.utils.data import DataLoader
 from perceptors import ImagebindPreceptor, LanguageBindPreceptor, UniBindPreceptor
 from dataloader import DATASETS
+from perceptors.base import FeatureStorage
 import h5py
 import json
 from tqdm import tqdm
@@ -25,6 +26,65 @@ def extract_features(dataset_class, preceptor_class, dataset_root, mode):
     with torch.no_grad():
         for data in tqdm(dataloader):
             outputs = preceptor(data[0])
+
+def save_classes(dataset_name, perceptor):
+    classes_set = set()
+    classes_map = {}
+    if "train" in DATASETS[dataset_name].MODES:
+        s, m = DATASETS[dataset_name](mode="train").get_classes()
+        classes_set = classes_set | s
+        classes_map.update(m)
+    if "test" in DATASETS[dataset_name].MODES:
+        s, m = DATASETS[dataset_name](mode="test").get_classes()
+        classes_set = classes_set | s
+        classes_map.update(m)
+        
+    classes_name = list(classes_set)
+    classes_name.sort()
+    from perceptors.base import FeatureStorage
+    
+    
+    class_feature_storage = FeatureStorage(f"{dataset_name}_class_{perceptor}.h5")
+    
+    if len(class_feature_storage.features) == len(classes_name):
+        return
+    
+    train_feature_storage = FeatureStorage(f"{dataset_name}_train_{perceptor}.h5")
+    test_feature_storage = FeatureStorage(f"{dataset_name}_test_{perceptor}.h5")
+
+    class_set = []
+    classes_ = []
+    class_features = {}
+    try:
+        text_pos =  DATASETS[dataset_name].MODALITIES.index(ModalityType.TEXT)
+    except:
+        text_pos = 0
+    for feature_storage in [train_feature_storage, test_feature_storage]:
+        for id, features in feature_storage.features.items():
+            flag=False
+            if id.startswith("tensor("):
+                id = id[7:-1]
+            for i in range(1,5):
+                sp_ = id.split("__")
+                sample_id = "__".join(sp_[:i])
+                if len(sp_) > i:
+                    sample_id += "_" if sp_[i][0] == "_" else ""
+                if sample_id in classes_map:
+                    class_ = classes_map[sample_id]
+                    classes_.append(classes_name.index(class_))
+                    if class_ not in class_set:
+                        class_set.append(class_) 
+                        class_features[class_] = features[text_pos]
+                    flag = True
+                    break
+            if not flag:
+                raise ValueError("")
+            
+    indices = class_features.keys()
+    features = [ class_features[i] for i in indices]
+    if dataset_name not in ["tvl"]:
+        assert len(indices) == len(classes_name), f"{dataset_name} {perceptor} {len(indices)} {len(classes_name)}"
+    class_feature_storage.save_features(features, indices)
 
 def build_tasks(gpus):
     datasets = {
@@ -98,3 +158,9 @@ if __name__ == "__main__":
     from fire import Fire
     Fire(main)
     
+"""
+# Extract Classes
+for d in DATASETS:
+    for perceptor in ["imagebind", "languagebind", "unibind"]:
+        save_classes(d, perceptor)
+"""
